09ff53cfd9397ee9908fcf435edc8a32b20d2526
Hilton Santos <jhilton930@gmail.com>
2023-06-29 23:58:52 -0300

    update nivel1fase1

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -41,8 +41,7 @@ public class GameManagerScript : MonoBehaviour
     {
         if (objectCorrect == totalObjects)
         {
-            Debug.Log("Finalizou");
-            ShowMessage("Nível Concluído", "Reiniciar");
+            ShowMessage("Fase Concluída", "Reiniciar");
         }
     }
 
